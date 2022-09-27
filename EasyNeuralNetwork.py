6df8944e7a615517f7a6ae0f33fef39b9b6e3378
C7H10N2 <111738870+C7H10N2@users.noreply.github.com>
2022-09-27 22:23:16 +0800

    update

--- EasyNeuralNetwork.py
+++ EasyNeuralNetwork.py
@@ -1,6 +1,3 @@
-from audioop import lin2adpcm
-from doctest import OutputChecker
-from turtle import window_height
 from numpy import random,dot,exp,array
 
 def fp(input):
@@ -9,13 +6,13 @@ def fp(input):
     return l1,l2
 
 def bp(y, l1, l2):
-    error = y - l2#误差
-    slpoe = l2 * (1 - l2)#斜率
-    l1_delta = error * slpoe #增量
+    error = y - l2#error
+    slpoe = l2 * (1 - l2)#slope
+    l1_delta = error * slpoe #delta
      
-    l0_error = l1_delta.dot(w1.T)#误差
-    l0_slpoe = l1 * (1 - l1)#斜率
-    l0_delta = l0_error * l0_slpoe #增量
+    l0_error = l1_delta.dot(w1.T)#error
+    l0_slpoe = l1 * (1 - l1)#slope
+    l0_delta = l0_error * l0_slpoe #delta
     return l0_delta, l1_delta
 
 #improt raw data
@@ -25,9 +22,9 @@ X = array([ [0,0,1],
             [1,1,1]])
 y = array([[0,0,1,1]]).T
 l0 = X
-#初始化权重
+#initialize the weights
 random.seed(1)
-w0 = random.random((3,10)) * 2 - 1 #随机0-1映射到（-1）-1
+w0 = random.random((3,10)) * 2 - 1 #Random 0-1 mapping to (-1)-1
 w1 = random.random((10,1)) * 2 - 1 
 
 for it in range(10000): 
@@ -35,4 +32,7 @@ for it in range(10000):
     l0_delta, l1_delta = bp(y,l1,l2)
     w0 = w0 + dot(l0.T, l0_delta)
     w1 = w1 + dot(l1.T, l1_delta)
+
+    
+#predicted
 print(fp([[1,0,0]])[1])
